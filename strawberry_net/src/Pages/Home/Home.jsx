642ab7fd15ee3886035e24a1b64375103c1f252f
Mahima <mahimakkar19071997@gmail.com>
2021-05-29 23:28:02 +0530

    navbar-initial

--- strawberry_net/src/Pages/Home/Home.jsx
+++ strawberry_net/src/Pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import React from "react"
 import { Banner } from "../../Components/HomeComponents/Banner"
-
+import { NavBar } from "../../Components/HomeComponents/NavBar"
 export const Home = () => {
     return (
         <div>
+            <NavBar/>
             <Banner/>
         </div>
     )
