89d29d45c767a316b38e0f4fc632f37ab1db7549
Mahima <mahimakkar19071997@gmail.com>
2021-05-29 23:36:22 +0530

    footer-initial

--- strawberry_net/src/Pages/Home/Home.jsx
+++ strawberry_net/src/Pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react"
 import { Banner } from "../../Components/HomeComponents/Banner"
+import { Footer } from "../../Components/HomeComponents/Footer"
 import { NavBar } from "../../Components/HomeComponents/NavBar"
 export const Home = () => {
     return (
         <div>
             <NavBar/>
             <Banner/>
+            <Footer/>
         </div>
     )
 }
