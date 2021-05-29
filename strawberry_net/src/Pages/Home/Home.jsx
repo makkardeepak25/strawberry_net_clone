a06e3f1f4653b0abae8af0a21fc928cc4c0f55ee
makkardeepak25 <makkardeepak25@gmail.com>
2021-05-29 22:55:26 +0530

    Home Page Initial

---
+++ strawberry_net/src/Pages/Home/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react"
+import { Banner } from "../../Components/HomeComponents/Banner"
+
+export const Home = () => {
+    return (
+        <div>
+            <Banner/>
+        </div>
+    )
+}
