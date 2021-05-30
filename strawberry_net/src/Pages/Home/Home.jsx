5a26022906c33d54b2a8dbb6a37e84b8ab7df364
makkardeepak25 <makkardeepak25@gmail.com>
2021-05-30 15:02:11 +0530

    Home Page 80%

--- strawberry_net/src/Pages/Home/Home.jsx
+++ strawberry_net/src/Pages/Home/Home.jsx
@@ -1,66 +1,139 @@
-import React from "react"
+import React from "react";
 import axios from "axios";
-import { Banner } from "../../Components/HomeComponents/Banner"
-import { Footer } from "../../Components/HomeComponents/Footer"
-import { NavBar } from "../../Components/HomeComponents/NavBar"
-import styles from "./Home.module.css"
-import { Card } from "../../Components/Product_card/Card";
+import { Banner } from "../../Components/HomeComponents/Banner";
+import { Footer } from "../../Components/HomeComponents/Footer";
+import { NavBar } from "../../Components/HomeComponents/NavBar";
+import styles from "./Home.module.css";
+import { DailySpecial } from "../../Components/HomeComponents/DailySpecial";
+import { BestDeals } from "../../Components/HomeComponents/BestDeals";
+import { Top40 } from "../../Components/HomeComponents/Top40";
+import { BestSellers } from "../../Components/HomeComponents/BestSellers";
+import { NewArrivals } from "../../Components/HomeComponents/NewArrivals";
 export const Home = () => {
-    const [data, setData] = React.useState([]);
-    const getData = () => {
-      axios.get("https://6wwnt.sse.codesandbox.io/products").then(res => {
-        console.log(res.data);
-        setData(res.data);
-      });
-    };
-    React.useEffect(() => {
-      getData();
-    }, []);
-    return (
-        <div>
-            <NavBar />
-            <br></br><br></br>
-            <Banner/>
-            <div className={styles.dailyspecial} >
-                <div className={styles.promopadding}>
-                    <div className={styles.promotitleline}></div>
-                    <div className={styles.promotitle}>
-                        <div className={styles.promotitlediv}>
-                            <h3>Daily Specials</h3>
-                            <div className={styles.dailymessage}>
-                                <span className={styles.dailypromocounter}>Ends in</span>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={styles.textright}><span className={styles.maxitem}>Max. 3 per order</span></div>
-                </div>
-                <div className={styles.productgridlist}>
-                {data.slice(0,8).map(el => {
-                    return <Card {...el} key={el.id}/>
-                })}
+  return (
+    <div>
+      <NavBar />
+      <br />
+      <br />
+      <Banner />
+      <div className={styles.dailyspecial}>
+        <div className={styles.promopadding}>
+          <div className={styles.promotitleline} />
+          <div className={styles.promotitle}>
+            <div className={styles.promotitlediv}>
+              <h3>Daily Specials</h3>
+              <div className={styles.dailymessage}>
+                <span className={styles.dailypromocounter}>Ends in</span>
+              </div>
             </div>
-            </div>
-            <div className={styles.containerfluid}>
-                <div className={styles.promoblockpicks}>
-                    <div className={styles.container}>
-                        <div className={styles.promblock}>
-                            <div className={styles.promotitlepad}>
-                                <div className={styles.promotitleline}></div>
-                                <div className={styles.promotitle}>
-                                    <div className={styles.promotitledeals}>
-                                        <h3>Best Deals</h3>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-         
-           
-           
-            <Footer/>
+          </div>
+          <div className={styles.textright}>
+            <span className={styles.maxitem}>Max. 3 per order</span>
+          </div>
         </div>
-    )
-}
+        <div className={styles.productgridlist}>
+          <DailySpecial />
+        </div>
+      </div>
+      <div className={styles.containerfluid}>
+        <div className={styles.promoblockpicks}>
+          <div className={styles.container}>
+            <div className={styles.promblock}>
+              <div className={styles.promotitlepad}>
+                <div className={styles.promotitleline} />
+                <div className={styles.promotitle}>
+                  <div className={styles.promotitledeals}>
+                    <h3>Best Deals</h3>
+                  </div>
+                </div>
+              </div>
+              <div className={styles.productgridlist}>
+                <BestDeals />
+              </div>
+              <div className={styles.textright}>
+                <button className={styles.bestbtn}>
+                  View All <span className={styles.arrclr}>></span>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.promblock}>
+          <div className={styles.promotitlepad}>
+            <div className={styles.promotitleline} />
+            <div className={styles.promotitle}>
+              <div className={styles.promotitledeal}>
+                <h3>TOP 40 SPECIALS</h3>
+              </div>
+            </div>
+          </div>
+          <div className={styles.productgridlist}>
+            <Top40 />
+          </div>
+          <div className={styles.textright}>
+            <button className={styles.bestbtn}>
+              View All <span className={styles.arrclr}>></span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.promblock}>
+          <div className={styles.promotitlepad}>
+            <div className={styles.promotitleline} />
+            <div className={styles.promotitle}>
+              <div className={styles.promotitledeal}>
+                <h3>Best Sellers</h3>
+              </div>
+            </div>
+          </div>
+          <div className={styles.productgridlist}>
+            <BestSellers />
+          </div>
+          <div className={styles.textright}>
+            <button className={styles.bestbtn}>
+              View All <span className={styles.arrclr}>></span>
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <div className={styles.promblock}>
+          <div className={styles.promotitlepad}>
+            <div className={styles.promotitleline} />
+            <div className={styles.promotitle}>
+              <div className={styles.promotitledeal}>
+                <h3>New Arrivals</h3>
+              </div>
+            </div>
+          </div>
+          <div className={styles.productgridlist}>
+            <NewArrivals />
+          </div>
+          <div className={styles.textright}>
+            <button className={styles.bestbtn}>
+              View All <span className={styles.arrclr}>></span>
+            </button>
+          </div>
+        </div>
+          </div>
+          <div className={styles.container}>
+        <div className={styles.promblock}>
+          <div className={styles.promotitlepad}>
+            <div className={styles.promotitleline} />
+            <div className={styles.promotitle}>
+              <div className={styles.promotitledeal}>
+                <h3>You may like these</h3>
+              </div>
+            </div>
+          </div>
+         
+        </div>
+      </div>
+
+      <Footer />
+    </div>
+  );
+};
