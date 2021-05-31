4dd08fbe7b2e7defd771c5d58e5c88df7530ed47
Mahima <mahi19071997@gmail.com>
2021-05-31 16:45:50 +0530

    Navbar changes

--- strawberry_net/src/Components/HomeComponents/NavBar.jsx
+++ strawberry_net/src/Components/HomeComponents/NavBar.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import styles from "./NavBar.module.css";
 import { BsSearch } from "react-icons/bs";
 import { AiOutlineDown } from "react-icons/ai";
-import {GiHamburgerMenu} from "react-icons/gi"
+import { GiHamburgerMenu } from "react-icons/gi";
 import { FaUserCircle } from "react-icons/fa";
 import { IconContext } from "react-icons";
-import {AiFillHeart} from "react-icons/ai"
-import {GiShoppingBag} from "react-icons/gi"
-import {Link} from "react-router-dom"
+import { AiFillHeart } from "react-icons/ai";
+import { GiShoppingBag } from "react-icons/gi";
+import { Link } from "react-router-dom";
+import { BsFillGiftFill } from "react-icons/bs";
+import { AiFillStar } from "react-icons/ai";
 export const NavBar = () => {
   return (
     <div className={styles.navbar}>
@@ -32,45 +34,35 @@ export const NavBar = () => {
               <li className={styles.loginlinks}>
                 <a className={styles.accname} href="#">
                   <IconContext.Provider
-                    value={{ color: "#b53788",size:"3.2em" }}
+                    value={{ color: "#b53788", size: "3.2em" }}
                   >
-                   
-                      < FaUserCircle />
-                   
+                    <FaUserCircle />
                   </IconContext.Provider>
                   <span className={styles.aaccname}>Sign in</span>
                 </a>
               </li>
 
-
               <li className={styles.loginlinks}>
                 <a className={styles.accname} href="#">
                   <IconContext.Provider
-                    value={{ color: "#b53788",size:"3.2em" }}
+                    value={{ color: "#b53788", size: "3.2em" }}
                   >
-                   
-                      < AiFillHeart />
-                   
+                    <AiFillHeart />
                   </IconContext.Provider>
                   <span className={styles.aaccname}>Wishlist</span>
                 </a>
               </li>
 
-
               <li className={styles.loginlinks}>
                 <a className={styles.accname} href="#">
                   <IconContext.Provider
-                    value={{ color: "#b53788",size:"3.2em" }}
+                    value={{ color: "#b53788", size: "3.2em" }}
                   >
-                   
-                      < GiShoppingBag />
-                   
+                    <GiShoppingBag />
                   </IconContext.Provider>
                   <span className={styles.aaccname}>Bag</span>
                 </a>
               </li>
-
-
             </ul>
           </div>
           <div className={styles.navform}>
@@ -89,15 +81,89 @@ export const NavBar = () => {
         </div>
       </div>
       <div className={styles.navbarmenu}>
-        <ul className={styles.menucont}>
-         <Link> <li><span><IconContext.Provider
-                    value={{ color: "white",size:"2em" }}
-                  >
-                   
-                      < GiHamburgerMenu />
-                   
-                  </IconContext.Provider></span><span>Shop by brand</span></li></Link>
-        </ul>
+        <div className={styles.menucont}>
+          <Link to={"/brands"}>
+            {" "}
+            <div className={styles.li1}>
+              <span>
+                <IconContext.Provider value={{ color: "white", size: "2em" }}>
+                  <GiHamburgerMenu />
+                </IconContext.Provider>
+              </span>
+              <span className={styles.clrspns}>SHOP BY BRAND</span>
+            </div>
+          </Link>
+          <Link to={"/skincare"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>SKINCARE</span>
+            </div>
+          </Link>
+          <Link>
+            {" "}
+            <div to={"/makeup"}>
+              <span className={styles.clrspn}>MAKEUP</span>
+            </div>
+          </Link>
+          <Link to={"/haircare"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>HAIRCARE</span>
+            </div>
+          </Link>
+          <Link to={"/perfume"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>PERFUME</span>
+            </div>
+          </Link>
+          <Link to={"/men-skincare"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>MEN'S SKINCARE</span>
+            </div>
+          </Link>
+          <Link to={"/men-cologne"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>MEN'S COLOGNE</span>
+            </div>
+          </Link>
+          <Link to={"/home-scents"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>HOME SCENTS</span>
+            </div>
+          </Link>
+          <Link to={"/natural-beauty"}>
+            {" "}
+            <div>
+              <span className={styles.clrspn}>NATURAL BEAUTY</span>
+            </div>
+          </Link>
+          <Link to={"/specials"}>
+            {" "}
+            <div className={styles.li1}>
+              <span>
+                <IconContext.Provider value={{ color: "white", size: "2em" }}>
+                  <BsFillGiftFill />
+                </IconContext.Provider>
+              </span>
+              <span className={styles.clrspns}>SPECIALS</span>
+            </div>
+          </Link>
+          <Link to={"/new-products"}>
+            {" "}
+            <div className={styles.li1}>
+              <span>
+                <IconContext.Provider value={{ color: "white", size: "2em" }}>
+                  <AiFillStar />
+                </IconContext.Provider>
+              </span>
+              <span className={styles.clrspns}>NEW</span>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
