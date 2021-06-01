a96f676099e17076a294231d4597cb0b927656b6
makkardeepak25 <makkardeepak25@gmail.com>
2021-06-02 01:47:04 +0530

    Product changes

--- strawberry_net/src/Components/HomeComponents/BestDeals.jsx
+++ strawberry_net/src/Components/HomeComponents/BestDeals.jsx
@@ -2,6 +2,8 @@ import React from "react"
 import axios from "axios"
 import styles from "./Card.module.css"
 import { Link } from "react-router-dom"
+import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder";
+
 
 export const BestDeals = () => {
     const [data, setData] = React.useState([]);
@@ -25,11 +27,16 @@ export const BestDeals = () => {
                         <span> {el.size[0].size}</span>
                       </p>
                     </a>
-                    <img className={styles.imgProd} src={el.images[0]} />
+                      <img className={styles.imgProd} src={el.images[0]} />
+                      {el.offer ? <div className={styles.offers}>Save {el.offer}%</div>:null}
                     <div className={styles.prodprice}>Rs&nbsp;{el.size[0].price}</div>
                     <button className={styles.bagbtn}>Add to bag</button>
                     <div className={styles.extraoff}>Extra 8% Off on US$80</div>
-                  </div></Link>
+                    </div>
+                    <div className={styles.wishlist}>
+                <FavoriteBorderIcon style={{ color: "#662d91" }} />
+              </div>
+                    </Link>
                 })}
         
         </>
