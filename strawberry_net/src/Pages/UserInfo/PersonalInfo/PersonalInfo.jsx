05bda7fe1fdcc6fddae4aa185533d79c11500d60
kamal <kamaldro97@gmail.com>
2021-06-06 12:26:56 +0530

    added function of getting userdetails to redux auth

--- strawberry_net/src/Pages/UserInfo/PersonalInfo/PersonalInfo.jsx
+++ strawberry_net/src/Pages/UserInfo/PersonalInfo/PersonalInfo.jsx
@@ -5,10 +5,8 @@ import Avatar from '@material-ui/core/Avatar';
 import styles from './PersonalInfo.module.css';
 import styled from 'styled-components';
 import {countries} from '../countries';
-
 import { useDispatch,useSelector } from 'react-redux';
 import { getUserDetails } from '../../../Redux/Auth/authAction';
-
 
 
 const UserInput = styled.input`
@@ -17,11 +15,9 @@ const UserInput = styled.input`
     height: 34px;
     border: 1px solid #623381;
     color: #808080c3;
-
     ::placeholder{
         /* font-size:16px; */
     }
-
 
 `
 
@@ -68,10 +64,6 @@ export const PersonalInfo = ({onChange}) => {
    
     const {f_name,l_name,email,password,phone,birth_date,avatar,gender,points,addresses,wishlisht,bag,orders,saved_cards } =initUser
     console.log(formData)
-
-  
-export const PersonalInfo = ({onChange}) => {
-
     const [showButton,setShowButton] =React.useState(false)
     const [newPassword,setnewPassword] = React.useState(false)
 
@@ -94,7 +86,6 @@ export const PersonalInfo = ({onChange}) => {
     
     const classes = useStyles();
 
-
     const handleOnFocus=()=>{
         setShowButton(true)
     }
@@ -103,7 +94,6 @@ export const PersonalInfo = ({onChange}) => {
         setFormData({...formData,[e.target.name]:e.target.value})
     }
 
-
     return (
         <div className={styles.fullmainCont}>
 
@@ -111,9 +101,7 @@ export const PersonalInfo = ({onChange}) => {
         <div className={classes.root,styles.mainCont}>
             <Avatar className={classes.large}  alt="" src={"https://s.cdnsbn.com/images/profile/10364147.jpg?t=1622449131812"} />
             <div className={styles.UploadButton}>
-
             <input type="file" className={styles.customeFileInput} onFocus={handleOnFocus} />
-
            
            
             </div>
@@ -123,18 +111,14 @@ export const PersonalInfo = ({onChange}) => {
                 <div className={styles.labelCont}>
                     <label htmlFor="">First name</label>
                 </div>
-
                 <div className={styles.inputCont} ><UserInput value={f_name} name="f_name" onFocus={handleOnFocus} onChange={handleOnChange}></UserInput></div>
-
                 
             </div>
             <div className={styles.userCont}>
                 <div className={styles.labelCont}>
                     <label htmlFor="">Surname</label>
                 </div>
-
                 <div className={styles.inputCont}><UserInput onFocus={handleOnFocus} value={l_name}></UserInput></div>
-
                 
             </div>
             <div className={styles.userCont}>
@@ -142,9 +126,7 @@ export const PersonalInfo = ({onChange}) => {
                     <label htmlFor="">NickName</label>
                     <div>(Optional)</div>
                 </div>
-
                 <div className={styles.inputCont}><UserInput placeholder="" onFocus={handleOnFocus}></UserInput></div>
-
                 
             </div>
             <div className={styles.userCont}>
@@ -153,9 +135,7 @@ export const PersonalInfo = ({onChange}) => {
                 </div>
                 <div className={styles.inputCont} style={{display:'flex',alignItems:'center'}}>
                         <label htmlFor="" style={{width:'120px'}} >Country Code</label>
-
                         <select name="" id="" style={{width:'30%'}} className={styles.selectBox} onFocus={handleOnFocus}>
-
                 {
                     countries.map(item=>
                         <option value="">{item.label} +{item.phone}</option>
@@ -164,9 +144,7 @@ export const PersonalInfo = ({onChange}) => {
                     </select>
                     <div style={{width:'10%'}} ></div>
                         <label htmlFor="" style={{width:'70px'}} >Number</label>
-
                         <UserInput style={{width:'35%'}} onFocus={handleOnFocus} ></UserInput>
-
 
                 </div>
                 
@@ -177,9 +155,7 @@ export const PersonalInfo = ({onChange}) => {
                     <div>(Optional)</div>
                 </div>
                 <div className={styles.inputCont}>
-
                     <select name="" id="" style={{width:'100%'}} className={styles.selectBox} onFocus={handleOnFocus}>
-
                     <option value="">Please Select</option>
                         <option value="">Male</option>
                         <option value="">Female</option>
@@ -195,18 +171,14 @@ export const PersonalInfo = ({onChange}) => {
                 </div>
                 <div className={styles.inputCont} style={{display:'flex',alignContent:'space-between',alignItems:'center',}}>
                 <label htmlFor="" style={{width:'70px'}}>Day</label>
-
                         <select name="" id="" style={{width:'20%'}} className={styles.selectBox} onFocus={handleOnFocus}>
-
                             {
                                  days.map((item,i)=><option value="">{i+1}</option>)
                             }
                         </select>
                         <div style={{width:'25%'}} ></div>
                         <label htmlFor="" style={{width:'60px'}} >Month</label>
-
                         <select name="" id="" style={{width:'35%'}} className={styles.selectBox} onFocus={handleOnFocus}>
-
                             
                             {
                                 months.map((item)=><option value="">{item}</option>)
@@ -221,7 +193,6 @@ export const PersonalInfo = ({onChange}) => {
                 <div className={styles.labelCont}>
                     <label htmlFor="">Password</label>
                 </div>
-
                 {!newPassword?(<div className={styles.inputCont} style={{display:'flex'}}>
                     <div className={styles.passwordSpan}>{"**********"}</div>
                     <button className={styles.passwordBtn}  onClick={()=>setnewPassword(true)}>Change</button>
@@ -231,8 +202,7 @@ export const PersonalInfo = ({onChange}) => {
                                 <input className={styles.inputpassword} placeholder="Enter your password" type="password" name="" id="" />
                                 <input className={styles.inputpassword} placeholder="Re-Enter your password"  type="password" name="" id="" />
                            </div>
- 
-
+                          
                     )}
                 
             </div>
@@ -241,9 +211,7 @@ export const PersonalInfo = ({onChange}) => {
                     <label htmlFor="">Location</label>
                 </div>
                 <div className={styles.inputCont} > 
-
                 <select name="" id="" style={{width:'100%'}} className={styles.selectBox} onFocus={handleOnFocus}>
-
                 {
                     countries.map(item=>
                         <option value="">{item.label}</option>
