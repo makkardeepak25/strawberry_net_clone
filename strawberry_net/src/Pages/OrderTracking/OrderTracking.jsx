c6ae9c99c98e181ac717c498e76f0d83bd3c103e
kamal <kamaldro97@gmail.com>
2021-06-14 11:54:49 +0530

    added ortracking component

---
+++ strawberry_net/src/Pages/OrderTracking/OrderTracking.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import CustomizedSteppers from './demo'
+
+export const OrderTracking = () => {
+    return (
+        <div>
+            
+            <CustomizedSteppers/>
+        </div>
+    )
+}
