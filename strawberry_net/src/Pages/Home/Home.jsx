96f785a2e028389ac9a73348a9fdb6748ae44737
makkardeepak25 <makkardeepak25@gmail.com>
2021-06-14 22:57:34 +0530

    Homepage remake

--- strawberry_net/src/Pages/Home/Home.jsx
+++ strawberry_net/src/Pages/Home/Home.jsx
@@ -1,8 +1,6 @@
 import React, { useEffect } from "react";
 import axios from "axios";
 import { Banner } from "../../Components/HomeComponents/Banner";
-// import { Footer } from "../../Components/HomeComponents/Footer";
-// import { NavBar } from "../../Components/HomeComponents/NavBar";
 import styles from "./Home.module.css";
 import { DailySpecial } from "../../Components/HomeComponents/DailySpecial";
 import { BestDeals } from "../../Components/HomeComponents/BestDeals";
@@ -20,49 +18,43 @@ export const Home = () => {
 
   return (
     <div className={styles.home}>
-      {/* <NavBar /> */}
 
       <Banner />
-      <div className={styles.dailyspecial}>
-        <div className={styles.promopadding}>
-          <div className={styles.promotitleline} />
-          <div className={styles.promotitle}>
-            <div className={styles.promotitlediv}>
-              <h3>Daily Specials</h3>
-              <div className={styles.dailymessage}>
-                <span className={styles.dailypromocounter}>
-                  Ends in{" "}
-                  <strong style={{ color: "hotpink" }}>
-                    <Timer />
-                  </strong>
-                </span>
-              </div>
+      <div className={styles.offerdaily}>
+        <div className={styles.extraline}>
+          <div className={styles.line} />
+          <div className={styles.dailyspec}>
+            <h3>Daily Specials</h3>
+            <div className={styles.endsin}>
+              <span className={styles.dailypromocounter}>
+                Ends in{" "}
+                <strong style={{ color: "hotpink" }}>
+                  <Timer />
+                </strong>
+              </span>
             </div>
           </div>
-          <div className={styles.textright}>
-            <span className={styles.maxitem}>Max. 3 per order</span>
-          </div>
+          <div className={styles.line} />
+        </div>
+        <div className={styles.textright}>
+          <span className={styles.maxitem}>Max. 3 per order</span>
         </div>
         <div className={styles.productgridlist}>
           <DailySpecial />
         </div>
       </div>
-      <div className={styles.containerfluid}>
-        <div className={styles.promoblockpicks}>
-          <div className={styles.container}>
-            <div className={styles.promblock}>
-              <div className={styles.promotitlepad}>
-                <div className={styles.promotitleline} />
-                <div className={styles.promotitle}>
-                  <div className={styles.promotitledeals}>
-                    <h3>Best Deals</h3>
-                  </div>
-                </div>
-              </div>
-              <div className={styles.productgridlist}>
-                <BestDeals />
-              </div>
-              <div className={styles.textright}>
+      <div className={styles.dealsdaily}>
+        <div className={styles.extraline}>
+          <div className={styles.lines} />
+          <div className={styles.dailyspec}>
+            <h3>Best deals</h3>
+          </div>
+          <div className={styles.lines} />
+        </div>
+        <div className={styles.productgridlist}>
+          <BestDeals />
+        </div>
+        <div className={styles.textright}>
                 <button className={styles.bestbtn}>
                   View All{" "}
                   <span className={styles.arrclr}>
@@ -70,24 +62,19 @@ export const Home = () => {
                   </span>
                 </button>
               </div>
-            </div>
-          </div>
-        </div>
       </div>
-      <div className={styles.container}>
-        <div className={styles.promblock}>
-          <div className={styles.promotitlepad}>
-            <div className={styles.promotitleline} />
-            <div className={styles.promotitle}>
-              <div className={styles.promotitledeal}>
-                <h3>TOP 40 SPECIALS</h3>
-              </div>
-            </div>
+      <div className={styles.offerdaily}>
+        <div className={styles.extraline}>
+          <div className={styles.line} />
+          <div className={styles.dailyspec}>
+            <h3>TOP 40 SPECIALS</h3>
           </div>
-          <div className={styles.productgridlist}>
-            <Top40 />
-          </div>
-          <div className={styles.textright}>
+          <div className={styles.line} />
+        </div>
+        <div className={styles.productgridlist}>
+        <Top40 />
+        </div>
+        <div className={styles.textright}>
             <button className={styles.bestbtn}>
               View All{" "}
               <span className={styles.arrclr}>
@@ -95,22 +82,22 @@ export const Home = () => {
               </span>
             </button>
           </div>
-        </div>
       </div>
-      <div className={styles.container}>
-        <div className={styles.promblock}>
-          <div className={styles.promotitlepad}>
-            <div className={styles.promotitleline} />
-            <div className={styles.promotitle}>
-              <div className={styles.promotitledeal}>
-                <h3>Best Sellers</h3>
-              </div>
-            </div>
+      <div className={styles.offerdaily}>
+        <div className={styles.extraline}>
+          <div className={styles.line} />
+          <div className={styles.dailyspec}>
+            <h3>Best Sellers</h3>
           </div>
-          <div className={styles.productgridlist}>
+          <div className={styles.line} />
+        </div>
+        <div className={styles.textright}>
+          <span className={styles.maxitem}>Max. 3 per order</span>
+        </div>
+        <div className={styles.productgridlist}>
             <BestSellers />
           </div>
-          <div className={styles.textright}>
+        <div className={styles.textright}>
             <button className={styles.bestbtn}>
               View All{" "}
               <span className={styles.arrclr}>
@@ -118,22 +105,23 @@ export const Home = () => {
               </span>
             </button>
           </div>
-        </div>
       </div>
-      <div className={styles.container}>
-        <div className={styles.promblock}>
-          <div className={styles.promotitlepad}>
-            <div className={styles.promotitleline} />
-            <div className={styles.promotitle}>
-              <div className={styles.promotitledeal}>
-                <h3>New Arrivals</h3>
-              </div>
-            </div>
+      
+       <div className={styles.offerdaily}>
+        <div className={styles.extraline}>
+          <div className={styles.line} />
+          <div className={styles.dailyspec}>
+            <h3>New Arrivals</h3>
           </div>
-          <div className={styles.productgridlist}>
+          <div className={styles.line} />
+        </div>
+        <div className={styles.textright}>
+          <span className={styles.maxitem}>Max. 3 per order</span>
+        </div>
+        <div className={styles.productgridlist}>
             <NewArrivals />
           </div>
-          <div className={styles.textright}>
+        <div className={styles.textright}>
             <button className={styles.bestbtn}>
               View All{" "}
               <span className={styles.arrclr}>
@@ -141,23 +129,21 @@ export const Home = () => {
               </span>
             </button>
           </div>
-        </div>
       </div>
-      <div className={styles.container}>
-        <div className={styles.promblock}>
-          <div className={styles.promotitlepad}>
-            <div className={styles.promotitleline} />
-            <div className={styles.promotitle}>
-              <div className={styles.promotitledeal}>
-                <h3>You may like these</h3>
-              </div>
-            </div>
+
+       <div className={styles.offerdaily}>
+        <div className={styles.extraline}>
+          <div className={styles.line} />
+          <div className={styles.dailyspec}>
+            <h3>You may like these</h3>
           </div>
-          <div className={styles.maylike}>
-            <MayLike/>
-          </div>
+          <div className={styles.line} />
         </div>
+        <div className={styles.maylike}>
+            <MayLike />
+          </div>
       </div>
+
     </div>
   );
 };
